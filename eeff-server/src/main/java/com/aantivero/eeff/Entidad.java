16333febeafe416adc908831c9c00a74c612e192
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2018-01-03 16:18:50 -0300

    Se crea la clase entidad para alojar todas las entidades financieras

---
+++ eeff-server/src/main/java/com/aantivero/eeff/Entidad.java
@@ -0,0 +1,18 @@
+package com.aantivero.eeff;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class Entidad {
+    @Id @GeneratedValue
+    private Long id;
+    private String codigo;
+    private String denominacion;
+}
---
+++ eeff-server/src/main/java/com/aantivero/eeff/EntidadRepository.java
@@ -0,0 +1,6 @@
+package com.aantivero.eeff;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntidadRepository extends JpaRepository<Entidad, Long> {
+}
