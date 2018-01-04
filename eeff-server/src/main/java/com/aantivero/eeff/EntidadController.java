fdbefa1ab0fa5953c22cfcdec407cfacf337877a
alejandroantivero <alejandro.antivero@gmail.com>
2018-01-04 01:00:09 -0300

    habilitar los cors para la llamada de la UI

--- eeff-server/src/main/java/com/aantivero/eeff/EntidadController.java
+++ eeff-server/src/main/java/com/aantivero/eeff/EntidadController.java
@@ -1,5 +1,6 @@
 package com.aantivero.eeff;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class EntidadController {
     }
 
     @GetMapping("/bancos")
+    @CrossOrigin(origins = "http://localhost:3000")
     public Collection<Entidad> bancos() {
         return repository.findAll().stream().filter(this::isBanco).collect(Collectors.toList());
     }
